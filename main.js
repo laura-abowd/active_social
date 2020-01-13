4599e5e8dc7157031fa21a8af84bc82b0888056f
Hello Lala <laura.abowd@gmail.com>
2020-01-13 00:05:54 -0600

    added contact us link for scroll

---
+++ main.js
@@ -0,0 +1,17 @@
+ //select elements, acccess to these elements
+var submitButton = document.querySelector('.submit-button');
+var backdrop = document.querySelector('.modal');
+var modal = document.querySelector('.modal-content');
+
+function openModal() {
+  backdrop.style.display = 'block';
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+}
+
+submitButton.onclick = openModal;
+// backdrop.onclick = closeModal;
