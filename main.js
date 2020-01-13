f8789ab51d9ceacf591123522a9a81cb48943455
Hello Lala <laura.abowd@gmail.com>
2020-01-13 12:56:41 -0600

    changes from 6 to 4 videos

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
  //select elements, acccess to these elements
-var submitButton = document.querySelector('.submit-button');
-var backdrop = document.querySelector('.modal');
-var modal = document.querySelector('.modal-content');
+// var submitButton = document.querySelector('.submit-button');
+// var backdrop = document.querySelector('.modal');
+// var modal = document.querySelector('.modal-content');
 
-function openModal() {
-  backdrop.style.display = 'block';
-  modal.style.display = 'block';
-}
+// function openModal() {
+//   backdrop.style.display = 'block';
+//   modal.style.display = 'block';
+// }
 
-function closeModal() {
-  backdrop.style.display = 'none';
-  modal.style.display = 'none';
-}
+// function closeModal() {
+//   backdrop.style.display = 'none';
+//   modal.style.display = 'none';
+// }
 
-submitButton.onclick = openModal;
+// submitButton.onclick = openModal;
 // backdrop.onclick = closeModal;
+
+
